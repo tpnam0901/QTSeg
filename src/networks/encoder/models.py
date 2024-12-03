5ce738f8471e794d0e62c6029e7658bf275b8625
namphuongtran9196 <namphuongtran9196@gmail.com>
2024-12-03 13:27:25 +0700

    feat: add FPN Encoder module

---
+++ src/networks/encoder/models.py
@@ -0,0 +1,60 @@
+import torch.nn as nn
+from .modules import Conv, C2f, Concat, SPPF
+from configs.base import Config
+
+
+class FPNEncoder(nn.Module):
+    def __init__(self, cfg: Config):
+        "FPN Encoder architecture base on YOLOv8 backbone"
+        super().__init__()
+        n = cfg.n_channel
+
+        # YOLO modules
+        self.conv_0 = Conv(cfg.image_channel, n, 3, 2)
+        self.conv_1 = Conv(n, n * 2, 3, 2)
+        self.c2f_2 = C2f(n * 2, n * 2, 1, True)
+        self.conv_3 = Conv(n * 2, n * 4, 3, 2)
+        self.c2f_4 = C2f(n * 4, n * 4, 2, True)
+        self.conv_5 = Conv(n * 4, n * 8, 3, 2)
+        self.c2f_6 = C2f(n * 8, n * 8, 2, True)
+        self.conv_7 = Conv(n * 8, n * 16, 3, 2)
+        self.c2f_8 = C2f(n * 16, n * 16, 1, True)
+        self.sppf_9 = SPPF(n * 16, n * 16, 5)
+        self.upsample_10 = nn.modules.upsampling.Upsample(None, 2, "nearest")
+        self.concat_11 = Concat(1)
+        self.c2f_12 = C2f(n * 24, n * 8, 1)
+        self.upsample_13 = nn.modules.upsampling.Upsample(None, 2, "nearest")
+        self.concat_14 = Concat(1)
+        self.c2f_15 = C2f(n * 12, n * 4, 1)
+        self.conv_16 = Conv(n * 4, n * 4, 3, 2)
+        self.concat_17 = Concat(1)
+        self.c2f_18 = C2f(n * 12, n * 8, 1)
+        self.conv_19 = Conv(n * 8, n * 8, 3, 2)
+        self.concat_20 = Concat(1)
+        self.c2f_21 = C2f(n * 24, n * 16, 1)
+
+    def forward(self, x):
+        x = self.conv_0(x)
+        x = self.conv_1(x)
+        x = self.c2f_2(x)
+        x = self.conv_3(x)
+        c2f_4 = self.c2f_4(x)
+        x = self.conv_5(c2f_4)
+        c2f_6 = self.c2f_6(x)
+        x = self.conv_7(c2f_6)
+        x = self.c2f_8(x)
+        sppf_9 = self.sppf_9(x)
+        x = self.upsample_10(sppf_9)
+        x = self.concat_11([x, c2f_6])
+        c2f_12 = self.c2f_12(x)
+        x = self.upsample_13(c2f_12)
+        x = self.concat_14([x, c2f_4])
+        c2f_15 = self.c2f_15(x)
+        x = self.conv_16(c2f_15)
+        x = self.concat_17([x, c2f_12])
+        c2f_18 = self.c2f_18(x)
+        x = self.conv_19(c2f_18)
+        x = self.concat_20([x, sppf_9])
+        c2f_21 = self.c2f_21(x)
+
+        return c2f_15, c2f_18, c2f_21
---
+++ src/networks/encoder/modules.py
@@ -0,0 +1,160 @@
+import torch
+import torch.nn as nn
+
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = (
+            d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]
+        )  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation).
+    Ref: https://github.com/ultralytics/ultralytics/blob/70d4a3752eda44580209ac821ff2c70df17a41bb/ultralytics/nn/modules/conv.py
+    """
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(
+            c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False
+        )
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = (
+            self.default_act
+            if act is True
+            else act if isinstance(act, nn.Module) else nn.Identity()
+        )
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+
+class ConvTranspose(nn.Module):
+    """Convolution transpose 2d layer.
+    Ref: https://github.com/ultralytics/ultralytics/blob/70d4a3752eda44580209ac821ff2c70df17a41bb/ultralytics/nn/modules/conv.py
+    """
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=2, s=2, p=0, bn=True, act=True):
+        """Initialize ConvTranspose2d layer with batch normalization and activation function."""
+        super().__init__()
+        self.conv_transpose = nn.ConvTranspose2d(c1, c2, k, s, p, bias=not bn)
+        self.bn = nn.BatchNorm2d(c2) if bn else nn.Identity()
+        self.act = (
+            self.default_act
+            if act is True
+            else act if isinstance(act, nn.Module) else nn.Identity()
+        )
+
+    def forward(self, x):
+        """Applies transposed convolutions, batch normalization and activation to input."""
+        return self.act(self.bn(self.conv_transpose(x)))
+
+    def forward_fuse(self, x):
+        """Applies activation and convolution transpose operation to input."""
+        return self.act(self.conv_transpose(x))
+
+
+class Concat(nn.Module):
+    """Concatenate a list of tensors along dimension.
+    Ref: https://github.com/ultralytics/ultralytics/blob/70d4a3752eda44580209ac821ff2c70df17a41bb/ultralytics/nn/modules/conv.py
+    """
+
+    def __init__(self, dimension=1):
+        """Concatenates a list of tensors along a specified dimension."""
+        super().__init__()
+        self.d = dimension
+
+    def forward(self, x):
+        """Forward pass for the YOLOv8 mask Proto module."""
+        return torch.cat(x, self.d)
+
+
+class Bottleneck(nn.Module):
+    """Standard bottleneck.
+    Ref: https://github.com/ultralytics/ultralytics/blob/70d4a3752eda44580209ac821ff2c70df17a41bb/ultralytics/nn/modules/block.py
+    """
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a bottleneck module with given input/output channels, shortcut option, group, kernels, and
+        expansion.
+        """
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        """'forward()' applies the YOLO FPN to input data."""
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+
+class SPPF(nn.Module):
+    """Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher.
+    Ref: https://github.com/ultralytics/ultralytics/blob/70d4a3752eda44580209ac821ff2c70df17a41bb/ultralytics/nn/modules/block.py
+    """
+
+    def __init__(self, c1, c2, k=5):
+        """
+        Initializes the SPPF layer with given input/output channels and kernel size.
+
+        This module is equivalent to SPP(k=(5, 9, 13)).
+        """
+        super().__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * 4, c2, 1, 1)
+        self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
+
+    def forward(self, x):
+        """Forward pass through Ghost Convolution block."""
+        x = self.cv1(x)
+        y1 = self.m(x)
+        y2 = self.m(y1)
+        return self.cv2(torch.cat((x, y1, y2, self.m(y2)), 1))
+
+
+class C2f(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions.
+    Ref: https://github.com/ultralytics/ultralytics/blob/70d4a3752eda44580209ac821ff2c70df17a41bb/ultralytics/nn/modules/block.py
+    """
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(
+            Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0)
+            for _ in range(n)
+        )
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
