406ba6e153724f8989cf56ff04f036ef1b3c4195
namphuongtran9196 <namphuongtran9196@gmail.com>
2024-12-03 13:29:11 +0700

    feat: add optimizers custom

---
+++ src/networks/optimizers.py
@@ -0,0 +1,35 @@
+from torch import optim
+from configs.base import Config
+
+
+def adamw(train_params, cfg: Config):
+    optimizer = optim.AdamW(
+        params=train_params,
+        lr=cfg.learning_rate,
+        weight_decay=cfg.weight_decay,
+        betas=cfg.betas,
+        eps=cfg.eps,
+        amsgrad=cfg.amsgard,
+    )
+    return optimizer
+
+
+def sgd(train_params, cfg: Config):
+    optimizer = optim.SGD(
+        params=train_params,
+        lr=cfg.learning_rate,
+        momentum=cfg.momentum,
+        weight_decay=cfg.weight_decay,
+    )
+    return optimizer
+
+
+def adam(train_params, cfg: Config):
+    optimizer = optim.Adam(
+        params=train_params,
+        lr=cfg.learning_rate,
+        betas=cfg.betas,
+        eps=cfg.eps,
+        amsgrad=cfg.amsgard,
+    )
+    return optimizer
